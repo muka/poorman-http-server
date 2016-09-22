3445abed03766af6028ddd844b466709612e0b39
Luca Capra <luca.capra@gmail.com>
2016-09-22 17:19:03 +0200

    first version

---
+++ README.md
@@ -0,0 +1,22 @@
+#http-server
+
+Poorman HTTP static file server
+
+Install
+---
+
+`sudo npm i -g http-server`
+
+Usage
+---
+
+Default is `http-server` which is the same as `http-server ./ 3000`
+
+To server `./public` folder on port `8080`
+
+`http-server public 8080`
+
+License
+---
+
+MIT
--- bin/http-server.js
+++ bin/http-server.js
@@ -1,1 +1,57 @@
 #!/usr/bin/env node
+
+var fs = require('fs');
+
+var args = process.argv;
+
+var express = require('express');
+
+var dirArg = args[2];
+var portArg = args[3];
+
+if(parseInt(dirArg)+"" === dirArg) {
+  portArg = dirArg;
+  dirArg = "./"
+}
+
+var dir = dirArg ? dirArg : './'
+
+var path = require('path');
+dir = path.resolve(dir)
+
+if(!fs.existsSync(dir)) {
+  console.error("Directory does not exists: %s", dir);
+  process.exit(1);
+}
+
+var port = portArg ? portArg : '3000';
+var _p = port.split(':')
+
+var host = 'localhost';
+if(_p.length > 1) {
+  host = _p[0];
+  port = _p[1];
+}
+
+
+var index = [ 'index.html','index.htm', ];
+while(index.length) {
+
+  var idx = path.resolve('./' + index.pop());
+  if(fs.existsSync()) {
+    app.get('/', express.static(idx));
+    break;
+  }
+}
+
+var app = express();
+app.use(express.static(dir));
+try {
+  app.listen(port, host, function() {
+    console.log("Serving %s at http://%s:%s", dir, host, port);
+  });
+}
+catch(e) {
+  console.error(e.message);
+  process.exit(1);
+}
